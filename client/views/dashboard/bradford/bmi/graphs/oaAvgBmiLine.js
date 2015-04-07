3dfbf86c20e8d6b484ff194c10a19a68e89659eb
saifallah786@gmail.com <saifallah786@gmail.com>
2015-04-07 16:49:08 +0000

    start of final restructuring

--- client/views/dashboard/bradford/bmi/graphs/oaAvgBmiLine.js
+++ client/views/dashboard/bradford/bmi/graphs/oaAvgBmiLine.js
@@ -1,104 +1,11 @@
-// //https://github.com/MaazAli/Meteor-HighCharts/issues/9
-//   var bmis = Session.get('bmiHistoryData');
-// //     var bmis = OaAvgBmiHistory.find().fetch();
-// //     console.log(Session.get('bmiHistoryData'));
-//     var dataset = [];
-//     var bmisOnly = [];
-//     var datesOnly = [];
- 
-// function getData() {
-//   Remote.call('oaAvgBmiHistory', function(error, result) {
-//       Session.set('bmiHistoryData', result);
-//     });
-
-//   if(Session.get('bmiHistoryData') !== undefined){
-//     Session.get('bmiHistoryData').forEach(function(record) {
-//       var item = {
-//         "date": record.updatedAt,
-//         "avgBmi": record.avgBmi
-//       };
-      
-// //       var item2 = {
-// //         "weight": weight.weight
-// //       };
-   
-//       dataset.push(item);
-//       bmisOnly.push(record.avgBmi);
-//       datesOnly.push(moment(record.updatedAt).format("DD/MM/YYYY"));
+function buildAvgBmiChartReactive() {
     
-//     });
-//     }
-// }
-// function buildGraph() {
-//   return new Highcharts.Chart({
-//     chart: {
-//               renderTo: 'OaAvgBmiLine',
-//               plotBackgroundColor: null,
-//               plotBorderWidth: null,
-//               plotShadow: false
-//           },
-//           title: {
-//               text: 'Overall Average BMI\'s',
-//               align: 'center',
-//               x: -20 //center
-//           },
-//           subtitle: {
-//               text: '',
-//             margin: 30,
-//               x: -20
-//           },
-//           xAxis: {
-//               categories: datesOnly,
-//   //             labels: {
-//   //               align
-
-//   //           }
-//   //           ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-//   //                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-//           },
-//           yAxis: {
-//               title: {
-//                   text: 'BMI'
-//               },
-//               plotLines: [{
-//                   value: 0,
-//                   width: 1,
-//                   color: '#808080'
-//               }]
-//           },
-//           tooltip: {
-//               valueSuffix: ''
-//           },
-//           legend: {
-//               layout: 'vertical',
-//               align: 'center',
-//               verticalAlign: 'bottom',
-//               floating: false,
-//               margin: 15,
-//               borderWidth: 0
-//           },
-//           series: [{
-//               name: 'Overall Average BMI\'s Daily',
-//               data: bmisOnly
-//           }]
-//       });
-//   }
-Template.oaAvgBmiLine.rendered = function() {
-  Session.setDefault('biHistoryData', null);
-//   var graph = buildGraph();
-  this.autorun(function() {
-//     graph.series[0].setData.
-      Remote.call('oaAvgBmiHistory', function(error, result) {
-      Session.set('bmiHistoryData', result);
-    });
-  });
-};
-Template.oaAvgBmiLine.helpers({
-  oaAvgBmiLine: function() {
+    var data = new Array();
     
-  },
-  oaAvgBmiHistChart: function() {
-    Remote.call('oaAvgBmiHistory', function(error, result) {
+    data[0] = 0;
+//     var bmi = Session.get('latestUserBmiByPCode')
+//   console.log(bmi);
+  Remote.call('oaAvgBmiHistory', function(error, result) {
       Session.set('bmiHistoryData', result);
     });
 //     OaAvgBmiHistory = new Meteor.Collection('oaAvgBmiHist', Remote);
@@ -115,7 +22,7 @@ Template.oaAvgBmiLine.helpers({
     Session.get('bmiHistoryData').forEach(function(record) {
       var item = {
         "date": record.updatedAt,
-        "avgBmi": record.avgBmi
+        "avgBmi": record.avgBmi.toFixed(2)
       };
       
 //       var item2 = {
@@ -123,20 +30,23 @@ Template.oaAvgBmiLine.helpers({
 //       };
    
       dataset.push(item);
-      bmisOnly.push(record.avgBmi);
+      bmisOnly.push(Math.round(record.avgBmi));
+      console.log(bmisOnly);
       datesOnly.push(moment(record.updatedAt).format("DD/MM/YYYY"));
     
     });
     }
-//     else{
-//       Tracker.flush();
-//     }
-  console.log(JSON.stringify(dataset));
-  var bmisAsJSON = JSON.stringify(dataset);
-  var bmisOnlyAsJSON = JSON.stringify(bmisOnly);  
-  console.log(datesOnly);  
-    graphObject = {
-      chart: {
+  
+//     if(Session.get("latestUserBmisByPCode") !== undefined)
+//       data[0] = Math.round(Session.get("latestUserBmisByPCode").avgBmi);
+      
+//     if(Session.get('reactive') !== undefined)
+//         data[0] = Session.get('reactive');
+  
+  console.log(data[0]);  
+  
+    $('#OaAvgBmiLine').highcharts({
+        chart: {
         plotBackgroundColor: null,
         plotBorderWidth: null,
         plotShadow: false
@@ -185,8 +95,15 @@ Template.oaAvgBmiLine.helpers({
         name: 'Overall Average BMI\'s Daily',
         data: bmisOnly
       }]
-    };
-//     if(Session.get('bmiHistoryData') !== undefined){
-    return graphObject;
-//     }
-  }});
+    });
+}
+
+
+
+
+Template.oaAvgBmiLine.rendered = function () {
+    this.autorun(function (c) {
+//       console.log(Session.get('latestUserBmisByPCode'));  
+      buildAvgBmiChartReactive();
+    });
+}
